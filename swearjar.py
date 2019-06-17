207764cae9ecd735c2e73c3fd467074ffa2fadd9
Moo Park <amp0303@gmail.com>
2019-06-16 23:42:25 -0700

    Second commit

--- swearjar.py
+++ swearjar.py
@@ -1,9 +1,5 @@
 import tweepy
 
-consumer_key = '6YOHP51iK3EwFTH1gBKlM8L2q'
-consumer_secret = 'ObafnJUoLwnRR8TxSMyN0SrF5q3AnomHDNvtjx3BTDAb0Ta45a'
-access_token = '1140328953929801728-IDWvSF2PE0qWjqTXfEdDWsjijef2bp'
-access_token_secret = 'au7LzwTX5k5x4PWv3z2Z6WKdgLqWD4qZaeb9uRUgRmubz'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
