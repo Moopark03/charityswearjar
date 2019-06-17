dc9e895aa83665cc25b5e879228901cd60372a57
Moo Park <amp0303@gmail.com>
2019-06-16 23:53:49 -0700

    removed API keys

--- swearjar.py
+++ swearjar.py
@@ -1,10 +1,16 @@
 import tweepy
+import authentication
 
+consumer_key = authentication.consumer_key
+consumer_secret = authentication.consumer_secret
+access_token = authentication.access_token
+access_token_secret = authentication.access_token_secret
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 user = api.me()
+
 
 for follower in tweepy.Cursor(api.followers).items():
     follower.follow()
@@ -16,10 +22,10 @@ for follower in tweepy.Cursor(api.followers).items():
 #f.close()
 #words contain all the keywords to search for
 
-phrase = 'Bad words are not as bad as being selfish. Donate to a charity of the week: https://www.plannedparenthood.org/get-involved/other-ways-give'
-numberOfTweets = 1
+
+#numberOfTweets = 1
 for tweet in tweepy.Cursor(api.search, q=('fuck OR shit OR motherfucker OR cunt OR shithead OR fucking -filter:retweets -filter:replies'), lang='en', page=2).items(numberOfTweets):
     tweet.retweet()
 
 #Things to work on:
-### 1. Scan tweets older than 1 hour
+# 1. Scan tweets older than 1 hour
